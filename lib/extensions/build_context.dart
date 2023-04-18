accb21f13f7c234c4eb6a26bb08eeb39a2f6ff9c
Simone Colazzo <s.colazzo@almaviva.it>
2023-04-18 17:57:40 +0200

    Fix on rtl

--- lib/extensions/build_context.dart
+++ lib/extensions/build_context.dart
@@ -7,4 +7,9 @@ extension DarkMode on BuildContext {
     final brightness = MediaQuery.of(this).platformBrightness;
     return brightness == Brightness.dark;
   }
+
+  bool isRtl() {
+    final directionality = Directionality.of(this);
+    return directionality == TextDirection.rtl;
+  }
 }
